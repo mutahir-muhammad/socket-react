7689857d51b959a8ad78e07a5fd985935469c1d0
Mutahir <mutahir.khan44@yahoo.com>
2023-11-17 23:15:51 +0500

    Chat-app basic server ready

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const {Server} = require("socket.io");
+const cors = require("cors");
+const PORT = 4000;
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log(`User connected is ${socket.id}`)
+    socket.on("send_message", (data) => {
+        socket.broadcast.on("receive_message", data)
+    })
+})
+
+server.listen(PORT, () => {
+    console.log(`SERVER RUNNING ON ${PORT}`)
+});
